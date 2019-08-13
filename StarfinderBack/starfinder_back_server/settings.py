a89a86d2cddf930175186be45c4f4aaf9feb8e13
Skirgus <Skirgus@gmail.com>
2019-08-13 23:43:03 +0300

    Добавлены параметры для развертывания

--- StarfinderBack/starfinder_back_server/settings.py
+++ StarfinderBack/starfinder_back_server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ce4vh2uz8r$r9ulx9sbdau1jx$$_p%l)^u9v4aca*m551&ynj&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['skirs.pythonanywhere.com']
 
 
 # Application definition
@@ -127,6 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
